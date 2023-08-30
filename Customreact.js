304a662f40dfc86da15c22f21005e0e2308adedc
Muhammad-waqas-moin <85788318+Muhammad-waqas-moin@users.noreply.github.com>
2023-08-30 13:23:11 +0500

    custom react

---
+++ Customreact.js
@@ -0,0 +1,32 @@
+function customRander(reactElement, container) {
+  /*
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+  */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    // console.log(prop);
+    prop !== "children"
+      ? domElement.setAttribute(prop, reactElement.props[prop])
+      : "";
+    // if (prop === "children") continue;
+  }
+  container.appendChild(domElement);
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google",
+};
+
+const container = document.querySelector("#root");
+
+customRander(reactElement, container);
